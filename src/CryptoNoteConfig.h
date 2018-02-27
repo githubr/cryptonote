9bba292e84f0c756ef79338c4fd5512b0d2afecc
Hari Suminto <hari@suminto.net>
2018-02-27 21:34:06 +1000

    Fixed p2p nodes

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "10.142.0.2:17230",
-  "10.142.0.3:17230",
+  "35.231.121.3:17230",
+  "35.227.33.125:17230",
 };
 
 struct CheckpointData {
